0995b04b4ae087e1117907a71c429d23e9a3aca8
Clark <thecluskin@gmail.com>
2021-01-18 12:43:59 -0500

    updated database connection to atlas

--- src/database.js
+++ src/database.js
@@ -1,25 +1,30 @@
-const ObjectId = require('mongodb').ObjectID;
+const ObjectId = require("mongodb").ObjectID;
 const MongoClient = require("mongodb").MongoClient;
 const url =
-  "mongodb+srv://towers:zwcY4uMOUczknaix@cluster0.scpoy.mongodb.net/towers?retryWrites=true&w=majority";
+  "mongodb+srv://towers:gBwcbRYhpWD111Vy@cluster0.scpoy.mongodb.net/towers?retryWrites=true&w=majority";
 
-let _db;
+let cachedClient = null;
+let cachedDb = null;
 
 module.exports = {
   connectToServer: function (callback) {
+    if (cachedClient && cachedDb) {
+      return { client: cachedClient, db: cachedDb };
+    }
     // noinspection JSIgnoredPromiseFromCall,JSCheckFunctionSignatures
     MongoClient.connect(
       url,
       { useNewUrlParser: true, useUnifiedTopology: true },
       function (err, client) {
-        _db = client.db("heroku_6k5jt957");
+        cachedClient = client;
+        cachedDb = client.db("towers");
         return callback(err);
       }
     );
   },
 
   getDb: function () {
-    return _db;
+    return cachedDb;
   },
   ObjectId,
 };
